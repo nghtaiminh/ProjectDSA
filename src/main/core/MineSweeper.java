fea7027ce45df1fd731202b558160da319336515
JavaKhangNguyen <khang2073@gmail.com>
2022-04-27 16:36:46 +0700

    Update MineSweeper, Main

--- src/main/core/MineSweeper.java
+++ src/main/core/MineSweeper.java
@@ -1,15 +1,11 @@
 package main.core;
-
 import main.enums.Difficulty;
-
 import java.util.Stack;
-
 public class MineSweeper {
     private static Difficulty level = Difficulty.EASY;
     private MineField board = new MineField(level);
-
     private Stack<MineField> undoStack = new Stack<>();
-
+    //undoStack.push(board) ?????
     public void undo() {
         if (!undoStack.empty()) {
             this.board = undoStack.pop();
