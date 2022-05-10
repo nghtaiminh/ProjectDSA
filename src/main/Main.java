d755c8506d41411a5b09cfc01269212eb6664a10
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-05-10 08:12:51 +0700

    gui for now

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 import main.utils.AudioPlayer;
 import main.graphics.GameGUI;
-public class Main {
+public class Main implements Runnable{
     private static AudioPlayer sound = new AudioPlayer();
-    public Main() {
-        GameGUI board = new GameGUI();
-    }
+    GameGUI gui = new GameGUI();
+    // public Main() {
+    //     GameGUI board = new GameGUI();
+    // }
     public static void main(String[] args) {
 	// write your code here
-    	
+    	new Thread(new Main()).start();
+    }
+
+    public void run(){
+        while(true){
+            gui.repaint();
+        }
     }
 }
--- src/main/graphics/GameGUI.java
+++ src/main/graphics/GameGUI.java
@@ -1,6 +1,8 @@
 package main.graphics;
 
 import javax.swing.*;
+import java.util.*;
+import java.awt.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
@@ -8,6 +10,37 @@ import java.awt.event.MouseListener;
 
 import javax.swing.Timer;
 
+import main.enums.Difficulty;
+
 public class GameGUI extends JFrame {
+
+    int spacing = 5;
+
+    public GameGUI(){
+        this.setTitle("Minesweeper");
+        this.setSize(1286,829);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        Board board = new Board();
+        this.setContentPane(board);
+    }
+
+    public class Board extends JPanel{
+        public void paintComponent (Graphics g){
+            g.setColor(Color.darkGray);
+            g.fillRect(0, 0, 1280, 800);
+            g.setColor(Color.gray);
+            for(int i = 0; i < Difficulty.HARD.getGridWidth(); i ++){
+                for(int j = 0; j < Difficulty.HARD.getGridHeight(); j++){
+                    g.fillRect(spacing+i*80, spacing+j*80+80, 80-2*spacing, 80-2*spacing);
+                }
+            }
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     new GameGUI();
+    // }
 
 }
