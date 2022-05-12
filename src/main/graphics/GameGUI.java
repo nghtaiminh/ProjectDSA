c2f8c8542c9486fed0fcbbbdbf64f62df38ecf88
ITITIU19020 <66402103+ITITIU19020@users.noreply.github.com>
2022-05-13 04:37:10 +0700

    little progress 13/5

--- src/main/graphics/GameGUI.java
+++ src/main/graphics/GameGUI.java
@@ -12,35 +12,78 @@ import javax.swing.Timer;
 
 import main.enums.Difficulty;
 
-public class GameGUI extends JFrame {
+import java.awt.event.ActionEvent;
 
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.Timer;
+
+public class GameGUI extends JFrame{
+    public JPanel introPanel;
+    public JButton easy,med,hard,buttonSample;
     int spacing = 5;
-
-    public GameGUI(){
-        this.setTitle("Minesweeper");
-        this.setSize(1286,829);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-
-        Board board = new Board();
-        this.setContentPane(board);
+  
+    GameGUI(){
+      this.setTitle("Minesweeper");
+      this.setSize(1286,829);
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setVisible(true);
+      this.setResizable(false);
+      createPanel();
+      addPanel();
     }
-
+  
+    private void createPanel()
+    {
+      introPanel = new JPanel();
+      introPanel.setBounds(0,0,500,500);
+      buttonSample = new JButton("Hello");
+      buttonSample.addActionListener(new addButtonListener());
+      buttonSample.setBounds(50, 90, 190, 30);
+  
+    }
+  
+    private void addPanel()
+    {
+        introPanel.add(buttonSample);
+        add(introPanel);
+    }
+  
+    class addButtonListener implements ActionListener
+    {
+      public void actionPerformed(ActionEvent ae) 
+      {
+          getContentPane().removeAll();
+          Board board = new Board();
+          setContentPane(board);//Adding to content pane, not to Frame
+          repaint();
+          printAll(getGraphics());//Extort print all content
+      }
+    }
     public class Board extends JPanel{
-        public void paintComponent (Graphics g){
-            g.setColor(Color.darkGray);
-            g.fillRect(0, 0, 1280, 800);
-            g.setColor(Color.gray);
-            for(int i = 0; i < Difficulty.HARD.getGridWidth(); i ++){
-                for(int j = 0; j < Difficulty.HARD.getGridHeight(); j++){
-                    g.fillRect(spacing+i*80, spacing+j*80+80, 80-2*spacing, 80-2*spacing);
-                }
-            }
-        }
+      public void paintComponent (Graphics g){
+          g.setColor(Color.darkGray);
+          g.fillRect(0, 0, 1280, 800);
+          g.setColor(Color.gray);
+          for(int i = 0; i < Difficulty.HARD.getGridWidth(); i ++){
+              for(int j = 0; j < Difficulty.HARD.getGridHeight(); j++){
+                  g.fillRect(spacing+i*40, spacing+j*40+40, 40-2*spacing, 40-2*spacing);
+              }
+          }
+      }
+  }
+  
+    public static void main(String[] args) {
+      new GameGUI();
     }
-
-    // public static void main(String[] args) {
-    //     new GameGUI();
-    // }
-
-}
+  }
