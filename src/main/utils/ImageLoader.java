bc02f967aa404d46df9f3d1f4148b9e6253daaf7
Minh Nguyen <ng.h.taiminh@gmail.com>
2022-05-29 13:33:44 +0700

    Update ImageLoader class

--- src/main/utils/ImageLoader.java
+++ src/main/utils/ImageLoader.java
@@ -10,23 +10,24 @@ public class ImageLoader {
     private HashMap<String, BufferedImage> listImage;
 
     public ImageLoader() {
+        String path = "src/main/assets/img/";
         listImage = new HashMap<String, BufferedImage>();
         try {
-            listImage.put("0", ImageIO.read(new File("src/main/assets/img/0.png")));
-            listImage.put("1", ImageIO.read(new File("src/main/assets/img/1.png")));
-            listImage.put("2", ImageIO.read(new File("src/main/assets/img/2.png")));
-            listImage.put("3", ImageIO.read(new File("src/main/assets/img/3.png")));
-            listImage.put("4", ImageIO.read(new File("src/main/assets/img/4.png")));
-            listImage.put("5", ImageIO.read(new File("src/main/assets/img/5.png")));
-            listImage.put("6", ImageIO.read(new File("src/main/assets/img/6.png")));
-            listImage.put("7", ImageIO.read(new File("src/main/assets/img/7.png")));
-            listImage.put("8", ImageIO.read(new File("src/main/assets/img/8.png")));
-            listImage.put("mine", ImageIO.read(new File("src/main/assets/img/mine.png")));
-            listImage.put("flag", ImageIO.read(new File("src/main/assets/img/flag.png")));
-            listImage.put("exploded_mine", ImageIO.read(new File("src/main/assets/img/exploded_mine.png")));
-            listImage.put("hidden", ImageIO.read(new File("src/main/assets/img/hidden.png")));
-            listImage.put("timer", ImageIO.read(new File("src/main/assets/img/timer.png")));
-            listImage.put("flag_icon", ImageIO.read(new File("src/main/assets/img/flag_icon.png")));
+            listImage.put("0", ImageIO.read(new File(path + "0.png").getAbsoluteFile()));
+            listImage.put("1", ImageIO.read(new File(path + "1.png").getAbsoluteFile()));
+            listImage.put("2", ImageIO.read(new File(path + "2.png").getAbsoluteFile()));
+            listImage.put("3", ImageIO.read(new File(path + "3.png").getAbsoluteFile()));
+            listImage.put("4", ImageIO.read(new File(path + "4.png").getAbsoluteFile()));
+            listImage.put("5", ImageIO.read(new File(path + "5.png").getAbsoluteFile()));
+            listImage.put("6", ImageIO.read(new File(path + "6.png").getAbsoluteFile()));
+            listImage.put("7", ImageIO.read(new File(path + "7.png").getAbsoluteFile()));
+            listImage.put("8", ImageIO.read(new File(path + "8.png").getAbsoluteFile()));
+            listImage.put("mine", ImageIO.read(new File(path + "mine.png").getAbsoluteFile()));
+            listImage.put("flag", ImageIO.read(new File(path + "flag.png").getAbsoluteFile()));
+            listImage.put("exploded_mine", ImageIO.read(new File(path + "exploded_mine.png").getAbsoluteFile()));
+            listImage.put("hidden", ImageIO.read(new File(path + "hidden.png").getAbsoluteFile()));
+            listImage.put("timer", ImageIO.read(new File(path + "timer.png").getAbsoluteFile()));
+            listImage.put("flag_icon", ImageIO.read(new File(path + "flag_icon.png").getAbsoluteFile()));
         }
         catch (IOException e) {
             e.printStackTrace();
